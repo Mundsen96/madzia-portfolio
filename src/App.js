fa94526da193debb91e485e02f09f0e7fe6b3899
Sebastian Młotkowski <sebamlot142@gmail.com>
2022-04-23 13:50:55 +0200

    First deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import About from './Components/About';
+import Footer from './Components/Footer';
+import Gallery from './Components/Gallery';
+import Navbar from './Components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <About></About>
+      <Gallery></Gallery>
+      <Footer></Footer>
     </div>
   );
 }
