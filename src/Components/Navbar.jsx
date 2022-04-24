a37fa4e5a02d7bce9fa53a10ac06c01b6f8e1c5f
Sebastian Młotkowski <sebamlot142@gmail.com>
2022-04-24 12:33:01 +0200

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,9 +13,9 @@ const Navbar = ()=>{
           <a href="#" className="hover:bg-red-300 rounded-full p-2 px-5 cursor-pointer">Contact</a>
         </div>
         <div className="hidden md:flex space-x-5 text-xl">
-        <a href="#" target='_blank'><i className="fab fa-pinterest"></i></a>
-        <a href="#" target='_blank'><i className="fab fa-instagram"></i></a>
-        <a href="#" target='_blank'><i className="fas fa-envelope-square"></i></a>
+        <a href="#" target='_blank' className="hover:animate-ping hover:scale-110"><i className="fab fa-pinterest"></i></a>
+        <a href="#" target='_blank' className="hover:animate-ping hover:scale-110"><i className="fab fa-instagram"></i></a>
+        <a href="#" target='_blank' className="hover:animate-ping hover:scale-110"><i className="fas fa-envelope-square"></i></a>
       </div>
       </div>
     </nav>
